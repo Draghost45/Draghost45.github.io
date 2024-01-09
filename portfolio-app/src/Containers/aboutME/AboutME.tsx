7c11d331d7475fbc2e52cc45ad8ee636d12240e4
Andrew Palacios <avargaspal23@gmail.com>
2024-01-08 19:51:21 -0600

    saving progress

---
+++ portfolio-app/src/Containers/aboutME/AboutME.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./aboutME.css";
+
+export const AboutME = () => {
+  return <div>AboutME</div>;
+};
+
+export default AboutME;
---
+++ portfolio-app/src/Containers/index.ts
@@ -0,0 +1,4 @@
+export {default as AboutME} from './aboutME/AboutME';
+export {default as Feature} from './features/Features';
+export {default as Footer} from './footer/Footer';
+export {default as Header} from './header/Header';

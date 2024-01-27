15a8c6ed352f81420986613ad5ab270b8be8bad9
Andrew Palacios <avargaspal23@gmail.com>
2024-01-26 18:55:08 -0600

    Finished with navigation bar

--- portfolio-app/src/Containers/header/Header.tsx
+++ portfolio-app/src/Containers/header/Header.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./header.css";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="portfolio_header section_padding" id="home">
+      <div className="portfolio_header-content">
+        <h1 className="gradient_text">My name is Andrew Vargas Palacios and Welcome to my Website!!! </h1>
+        
+        <div className="portfolio_header-content_input">
+        <input></input>  
+        </div>
+        <div/>
+        
+    </div>
+  );
 };
 
 export default Header;

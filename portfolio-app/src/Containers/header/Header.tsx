78ca3c58587d847a79c0c98d77b85a916ec8f4af
Andrew Palacios <avargaspal23@gmail.com>
2024-01-28 18:48:28 -0600

    working on header

--- portfolio-app/src/Containers/header/Header.tsx
+++ portfolio-app/src/Containers/header/Header.tsx
@@ -5,13 +5,16 @@ export const Header = () => {
   return (
     <div className="portfolio_header section_padding" id="home">
       <div className="portfolio_header-content">
-        <h1 className="gradient_text">My name is Andrew Vargas Palacios and Welcome to my Website!!! </h1>
-        
+        <h1 className="gradient_text">
+          My name is Andrew Vargas Palacios and Welcome to my Website!!!{" "}
+        </h1>
+        <p>Text will go here about me </p>
+
         <div className="portfolio_header-content_input">
-        <input></input>  
+          <input type="email" placeholder="Your Email Address"></input>
+          <button type="button">Get Started</button>
         </div>
-        <div/>
-        
+      </div>
     </div>
   );
 };

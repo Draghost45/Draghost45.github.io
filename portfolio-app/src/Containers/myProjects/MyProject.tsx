72bda7b87065081161fb76cad3002eb220aa86ec
Andrew Palacios <avargaspal23@gmail.com>
2024-05-10 18:06:13 -0500

    Updated work, updated on the projects information

--- portfolio-app/src/Containers/myProjects/MyProject.tsx
+++ portfolio-app/src/Containers/myProjects/MyProject.tsx
@@ -19,9 +19,18 @@ export const MyProject = () => {
         <p>Explore My Content</p>
       </div>
       <div className="portfolio_myproject-container">
-        <Feature title="Online Shopping Web Site " text="" />
-        <Feature title="Password Hassing with Salting " text="" />
-        <Feature title="" text="" />
+        <Feature
+          title="Online Shopping Web Site"
+          text="In this project, we developed and designed an interactive website utilizing HTML, CSS, and Javascript in collaboration with a team of four. This website allowed online users to shop for a small range of products and edit their shopping carts with ease. One of the main tasks I worked on was designing the login and signing-up pages for the online shopping website. One of the features we implemented was input validation to test for a valid username and password. "
+        />
+        <Feature
+          title="Password Hassing with Salting"
+          text="In the next project, I worked on a password hashing system for logging into a small database filled with employees. Furthermore, this program implemented a static salting method to strengthen security. I used a hashing function called the SHA-1 method to replicate the use of cryptography today. This includes the input validation of the user name and password, as well as checking if the user signs in with the correct username and password. "
+        />
+        <Feature
+          title="Predicting SUMOylation using Pre-trained Protein Language Models "
+          text=""
+        />
       </div>
     </div>
   );
